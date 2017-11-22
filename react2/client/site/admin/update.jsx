89e0df237da36172f454d2afb32a87ec16994cd9
Syndesi <soerenklein98@gmail.com>
2017-11-22 10:35:44 +0100

    changed the update-list a bit

--- react2/client/site/admin/update.jsx
+++ react2/client/site/admin/update.jsx
@@ -49,52 +49,63 @@ export default class Update extends React.Component {
     });
   }
 
+  getUpdateList(){
+    if(this.state.updates == null){
+      return null;
+    }
+    var list = [];
+    for(var i in this.state.updates){
+      var el = this.state.updates[i];
+      var liClass = 'list-group-item d-flex justify-content-between align-items-center';
+      var badgeClass = 'badge badge-pill';
+      if(this.state.currentUpdate == i){
+        liClass += ' active';
+        badgeClass += ' badge-light';
+      } else {
+        badgeClass += ' badge-primary';
+      }
+      list.push(
+        <li data-i={i} onClick={this.openUpdateDetail.bind(this)} className={liClass}>
+          {el.name}
+          <span className={badgeClass}>{el.tag_name}</span>
+        </li>
+      );
+    }
+    return list;
+  }
+
+  getUpdateDetail(){
+    if(this.state.updates == null){
+      return null;
+    }
+    var update = this.state.updates[this.state.currentUpdate];
+    var date = format('yyyy.MM.dd hh:mm', new Date(update.published_at));
+    var detail = (
+      <div className="card-body">
+        <a href={update.html_url}><h2>{update.name}</h2></a>
+        <p className="text-secondary"><small><em>
+          {date} by <a href={update.author.html_url} target="_blank" className="text-secondary"><u>{update.author.login}</u></a>
+        </em></small></p>
+        <Markdown text={update.body} />
+        <hr />
+        <div className="d-flex justify-content-between align-items-center">
+          <div className="btn-group" role="group" aria-label="download-links">
+            <div className="btn btn-secondary btn-sm"><span className="icon material">file_download</span></div>
+            <a href={update.zipball_url} className="btn btn-secondary btn-sm">zip</a>
+            <a href={update.tarball_url} className="btn btn-secondary btn-sm">tar.gz</a>
+          </div>
+          <div className="btn btn-primary">Update Server</div>
+        </div>
+      </div>
+    );
+    return detail;
+  }
+
   render() {
     var l = this.props.store.lang.site.admin.update;
     var state = this.state;
-    var updates = [];
-    var description = null;
-    if(state.updates != null){
-      for(var i in state.updates){
-        var el = state.updates[i];
-        var liClass = 'list-group-item d-flex justify-content-between align-items-center';
-        var badgeClass = 'badge badge-pill';
-        if(state.currentUpdate == i){
-          liClass += ' active';
-          badgeClass += ' badge-light';
-        } else {
-          badgeClass += ' badge-primary';
-        }
-        updates.push(
-          <li data-i={i} onClick={this.openUpdateDetail.bind(this)} className={liClass}>
-            {el.name}
-            <span className={badgeClass}>{el.tag_name}</span>
-          </li>
-        );
-      }
-      if(state.currentUpdate in state.updates){
-        var update = state.updates[state.currentUpdate];
-        var date = format('yyyy.MM.dd hh:mm', new Date(update.published_at));
-        description = (
-          <div className="card-body">
-            <a href={update.html_url}><h2>{update.name}</h2></a>
-            <p className="text-secondary"><small><em>
-              {date} by <a href={update.author.html_url} target="_blank" className="text-secondary"><u>{update.author.login}</u></a>
-            </em></small></p>
-            <Markdown text={update.body} />
-            <hr />
-            <div className="d-flex justify-content-between align-items-center">
-              <div className="btn-group" role="group" aria-label="download-links">
-                <div className="btn btn-secondary btn-sm"><span className="icon material">file_download</span></div>
-                <a href={update.zipball_url} className="btn btn-secondary btn-sm">zip</a>
-                <a href={update.tarball_url} className="btn btn-secondary btn-sm">tar.gz</a>
-              </div>
-              <div className="btn btn-primary">Update Server</div>
-            </div>
-          </div>
-        );
-      }
-    }
+    var updates = this.getUpdateList();
+    var detail = this.getUpdateDetail();
     return (
       <div className="container">
         <div className="row">
@@ -105,7 +116,7 @@ export default class Update extends React.Component {
           </div>
           <div className="col-md-8">
             <div className="card">
-              {description}
+              {detail}
             </div>
           </div>
         </div>
